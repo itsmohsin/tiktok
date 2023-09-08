cb692f3040177dad75dd290be15198e0157f5376
itsmohsin <mohsinsmannat@gmail.com>
2023-09-08 16:02:12 +0530

    Done with Add Video Screen

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,12 +3,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok/controllers/auth_contorller.dart';
+import 'package:tiktok/views/screens/add_video_screen.dart';
 
 // Pages:
 const pages = [
   Text('Home Screen'),
   Text('Search Screen'),
-  Text('Add Post Screen'),
+  AddVideoScreen(),
   Text('Message Screen'),
   Text('Profile Screen'),
 ];
