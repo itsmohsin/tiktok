2e3142ed219490dd2b9c34450f55d20003d1d700
itsmohsin <mohsinsmannat@gmail.com>
2023-09-08 16:45:01 +0530

    done with confirm video screen

--- lib/views/screens/add_video_screen.dart
+++ lib/views/screens/add_video_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:tiktok/constants.dart';
@@ -9,9 +11,14 @@ class AddVideoScreen extends StatelessWidget {
   pickVideo(ImageSource src, BuildContext context) async {
     final video = await ImagePicker().pickVideo(source: src);
     if (video != null) {
-      Navigator.of(context).push(MaterialPageRoute(
-        builder: (context) => ConfirmScreen(),
-      ));
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => ConfirmScreen(
+            videoFile: File(video.path),
+            videoPath: video.path,
+          ),
+        ),
+      );
     }
   }
 
