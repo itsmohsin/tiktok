812377a3a1af08194820b4d90018e4c331521a62
itsmohsin <mohsinsmannat@gmail.com>
2023-09-08 11:37:49 +0530

    done with firebase setup

--- lib/controllers/auth_contorller.dart
+++ lib/controllers/auth_contorller.dart
@@ -1,3 +1,44 @@
-import 'package:get/get_state_manager/get_state_manager.dart';
+import 'dart:io';
 
-class AuthController extends GetxController {}
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:get/get.dart';
+import 'package:tiktok/constants.dart';
+
+class AuthController extends GetxController {
+  // upload to firebase storage
+  Future<String> _uploadToStorage(File image) async {
+    Reference ref = firebaseStorage
+        .ref()
+        .child('profilePics')
+        .child(firebaseAuth.currentUser!.uid);
+
+    UploadTask uploadTask = ref.putFile(image);
+    TaskSnapshot snap = await uploadTask;
+    String downloadUrl = await snap.ref.getDownloadURL();
+    return downloadUrl;
+  }
+
+// register the user
+  Future<void> registerUser(
+      String username, String email, String password, File? image) async {
+    try {
+      if (username.isNotEmpty &&
+          email.isNotEmpty &&
+          password.isNotEmpty &&
+          image != null) {
+        // save our user to our authentication and firebase firestore
+        UserCredential cred = await firebaseAuth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        String downloadUrl = await _uploadToStorage(image);
+      }
+    } catch (e) {
+      Get.snackbar(
+        'Error Creating Account',
+        e.toString(),
+      );
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get/get.dart';
 import 'package:tiktok/constants.dart';
-import 'package:tiktok/views/screens/auth/login_screen.dart';
+import 'package:tiktok/controllers/auth_contorller.dart';
 import 'package:tiktok/views/screens/auth/signup_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp().then((value) {
+    Get.put(AuthController());
+  });
+
   runApp(const MyApp());
 }
 
