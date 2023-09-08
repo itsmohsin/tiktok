3e5f1363a9e7e69429c5ae9e54e8d14ea6ea5140
itsmohsin <mohsinsmannat@gmail.com>
2023-09-08 10:35:20 +0530

    Authentication Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:tiktok/constants.dart';
 import 'package:tiktok/views/screens/auth/login_screen.dart';
 import 'package:tiktok/views/screens/auth/signup_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -12,7 +16,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Tiktok Clone',
       theme: ThemeData.dark().copyWith(
